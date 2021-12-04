e22478a931231518a4bb6ce1c2dba41cb1869130
李潇 <iPhone4s2008@iCloud.com>
2021-12-04 14:25:49 +0800

    bugFix

--- init.sh
+++ init.sh
@@ -1,21 +1,21 @@
 GREEN="echo -e \e[32m"
 ${GREEN}"开启 autoremove"
-sudo apt autoremove
+sudo apt autoremove -y
 ${GREEN}"重新安装 vim"
-sudo apt-get remove vim-common
-sudo apt-get install vim
+sudo apt-get remove vim-common -y
+sudo apt-get install vim -y
 ${GREEN}"安装 git"
-sudo apt-get install git
+sudo apt-get install git -y
 ${GREEN}"创建 SSH Key"
 ssh-keygen -t rsa -C "iPhone4s2008@iCloud.com"
 git config --global user.name "李潇"
 git config --global user.email "iPhone4s2008@iCloud.com"
 ${GREEN}"安装 cmake"
-sudo apt-get install cmake
+sudo apt-get install cmake -y
 ${GREEN}"安装 g++"
-sudo apt install g++
+sudo apt install g++ -y
 ${GREEN}"安装 zsh"
-sudo apt install zsh
+sudo apt install zsh -y
 chsh -s /bin/zsh
 ${GREEN}"安装 oh my zsh"
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -25,7 +25,7 @@ wget http://mimosa-pudica.net/src/incr-0.2.zsh
 echo 'source ~/.oh-my-zsh/plugins/incr/incr*.zsh' >> ~/.zshrc
 source ~/.zshrc
 ${GREEN}"安装 autojump"
-sudo apt install autojump
+sudo apt install autojump -y
 echo 'source /usr/share/autojump/autojump.sh' >> ~/.zshrc
 source ~/.zshrc
 ${GREEN}"安装 zsh-syntax-highlighting"
